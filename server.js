c8eb3d275d7baff595f9df67ff78d4147e414b33
Jorge Pedret <jorge@chloi.io>
2013-06-16 21:08:41 -0700

    Got server.js running again

--- server.js
+++ server.js
@@ -2,7 +2,20 @@ var express     = require("express"),
     app         = express(),
     port        = process.env.PORT || 3000,
     config      = require("./test/mock/config.json"),
-    distillery  = require("./lib/distillery")(config, app);
+    Distillery  = require("./lib/distillery"),
+    helpers     = require("./test/mock/helpers"),
+    distillery;
 
+// Setup helper APIs so the examples work
+helpers.setupAPIs();
+
+// Instantiate and start distillery
+distillery = new Distillery(config, app);
+distillery.start();
+
+// Start express server
 app.listen(port);
-console.log("Distillery server is running on port " + port);
+console.log("Express + Distillery server is running on port " + port);
+
+// http://localhost:3000/api2/topic.json/bjj/followers
+// http://localhost:3000/api1/media/popular
