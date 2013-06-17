c22ad2b158bcac65460f0f67bfd90df9ba025dea
Jorge Pedret <jorge@chloi.io>
2013-06-17 00:46:00 -0700

    Implemented service reset method (deletes and re-creates the endpoint). Exposed an endpoint to reset the auth for a specific service (i.e.: PUT /api1/distillery/auth).

--- test/auth.js
+++ test/auth.js
@@ -1,8 +1,8 @@
 var request   = require("request"),
     should    = require("should"),
     config    = require("./mock/config.json"),
-    binteoUrl  = "http://localhost:3100/api2",
-    instagUrl = "http://localhost:3100/api1",
+    api2Url  = "http://localhost:3100/api2",
+    api1Url = "http://localhost:3100/api1",
     helpers = require("./mock/helpers"),
     Distillery = require("../lib/distillery"),
     distillery;
@@ -16,7 +16,7 @@ describe("auth", function () {
   });
   
   it("should get default auth", function (done) {
-    request.get(binteoUrl + "/topic.json/bjj/followers", function (err, res, body) {
+    request.get(api2Url + "/topic.json/bjj/followers", function (err, res, body) {
       var obj = {};
       should.not.exist(err);
       body.should.be.ok;
@@ -31,10 +31,52 @@ describe("auth", function () {
       }
     });
   });
+
+  it("should change auth through endpoint", function (done) {
+    var api2 = distillery.get('api2'),
+        api2auth = api2.get('auth'),
+        options = {
+          uri: api2Url + "/distillery/auth",
+          form: {
+            method: "get",
+            key: "token",
+            value: "123xyz"
+          }
+        };
+
+    api2auth.should.have.property('key', 'key');
+    api2auth.should.have.property('value', 'foobar');
+
+    request.put(options, function (err, res, body) {
+      var api2 = distillery.get('api2'),
+          api2auth = api2.get('auth');
+      api2auth.should.have.property('key', 'token');
+      api2auth.should.have.property('value', '123xyz');
+      done();
+    });
+    // done();
+  });
+
+  it("should use new auth values", function (done) {
+    var api2 = distillery.get('api2'),
+        api2auth = api2.get('auth');
+
+    request.get(api2Url + "/account.json/123", function (err, res, body) {
+      should.not.exist(err);
+      res.statusCode.should.not.eql(403);
+      try {
+        var data = JSON.parse(body);
+        data.should.be.a('object');
+        data.should.have.property('username', 'felipe');
+        done();
+      } catch (e) {
+        done(new Error("Endpoint returned invalida JSON data"));
+      }
+    });
+  });
   
   after(function (done) {
-    distillery.stop(function () {
-      helpers.teardownAPIs(done);
-    });
+    distillery.stop();
+    helpers.teardownAPIs(done);
   });
 });
